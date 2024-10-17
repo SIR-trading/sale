89b4ab954af43831380fe51b4904780267de3cc1
Codonyat <nil.garcia@gmail.com>
2024-10-17 15:46:52 +0200

    fix: specifiy chain id

--- script/RunAnvil.sh
+++ script/RunAnvil.sh
@@ -2,7 +2,7 @@
 source .env
 
 # Run the Anvil server
-anvil --chain-id 1337 -p $PORT --fork-url "https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_APIKEY}" --fork-block-number 20944804 &
+anvil --chain-id $CHAIN_ID -p $PORT --fork-url "https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_APIKEY}" --fork-block-number 20944804 &
 ANVIL_PID=$!
 
 # Impersonate automatically
